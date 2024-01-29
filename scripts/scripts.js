b06b6e0f489da5ce5f49c4313d51f72b5b50a959
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-01-29 01:17:50 +0000

    fixing styling / forecast icons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,102 +39,94 @@
                               }).then(function (data){
                                                                         
                                     var weather = data.current.weather[0].main;
-                                    console.log("Weather is " + weather);
-                                    
                                     // An icon representation of weather conditions
                                     var weatherIcon = data.current.weather[0].icon;
-                                    console.log(weatherIcon);
-                                    console.log(`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`)
-                                    
                                     // The temperature
                                     var temperature = data.current.temp;
-                                    console.log("Temperature is " + temperature);
-                                    
                                     // The humidity
                                     var humidity = data.current.humidity;
-                                    console.log("Humidity is " + humidity)
-                                    
                                     // The wind speed
                                     var windspeed = data.current.wind_speed;
-                                    console.log("Windspeed is " + windspeed);
 
                                     currentForecastarea.append(`
                                     <div class="card">
                                           <div class="card-body">
-                                          <h3 class="card-title">${cityName}</h3>
-                                          <h5>Today: ${date}</h5>
-                              
-                                          <div class="container d-flex flex-nowrap justify-content-start">
-                                          <div class="card m-2" style="width: 15rem;">
-                                                <div class="card-body">
-                                                <h5 class="card-title"><i class="me-2 bi bi-thermometer-half"></i> Temperature </h5>
-                                                <p class="card-text">${temperature}</p>
+                                                <div class="d-flex justify-content-between align-content-center">
+                                                      <div class="d-flex flex-column justify-content-evenly">
+                                                            <h3 class="card-title">${cityName}</h3>
+                                                            <h5>Today: ${date}</h5>
+                                                      </div>
+                                                      <img src="https://openweathermap.org/img/wn/${weatherIcon}@2x.png" alt="${weather}">
                                                 </div>
-                                          </div>
-                                          <div class="card m-2" style="width: 15rem;">
-                                                <div class="card-body">
-                                                <h5 class="card-title"><i class="me-2 bi bi-wind"></i> Wind</h5>
-                                                <p class="card-text">${windspeed}</p>
+                                                <div class="container d-flex flex-nowrap justify-content-start">
+                                                      <div class="card m-2" style="width: 15rem;">
+                                                            <div class="card-body">
+                                                            <h5 class="card-title"><i class="me-2 bi bi-thermometer-half"></i> Temperature </h5>
+                                                            <p class="card-text">${temperature}</p>
+                                                            </div>
+                                                      </div>
+                                                      <div class="card m-2" style="width: 15rem;">
+                                                            <div class="card-body">
+                                                            <h5 class="card-title"><i class="me-2 bi bi-wind"></i> Wind</h5>
+                                                            <p class="card-text">${windspeed}</p>
+                                                            </div>
+                                                      </div>
+                                                      <div class="card m-2" style="width: 15rem;">
+                                                            <div class="card-body">
+                                                            <h5 class="card-title"><i class="me-2 bi bi-droplet-half"></i> Humidity</h5>
+                                                            <p class="card-text">${humidity}</p>
+                                                            </div>
+                                                      </div>
                                                 </div>
-                                          </div>
-                                          <div class="card m-2" style="width: 15rem;">
-                                                <div class="card-body">
-                                                <h5 class="card-title"><i class="me-2 bi bi-droplet-half"></i> Humidity</h5>
-                                                <p class="card-text">${humidity}</p>
-                                                </div>
-                                          </div>
-                                          </div>
-                              
                                           </div>
                                     </div>
                                     `)
                               });
 
-                        var forecastDays = [currentDate.add(1, "day").valueOf(), currentDate.add(2, "day").valueOf(), currentDate.add(3, "day").valueOf(), currentDate.add(4, "day").valueOf(), currentDate.add(5, "day").valueOf()];
+                        var forecastDays = [currentDate.add(1, "day").format("YYYY-MM-DD"), currentDate.add(2, "day").format("YYYY-MM-DD"), currentDate.add(3, "day").format("YYYY-MM-DD"), currentDate.add(4, "day").format("YYYY-MM-DD"), currentDate.add(5, "day").format("YYYY-MM-DD")];
                         console.log(forecastDays)
                         
-                        
+                        fivedayforecastarea.append(`<div class="card"><div class="card-body"><h3 class="card-title" >5-Day Forecast</h3><div class="container d-flex flex-nowrap justify-content-start" id="forecastcards"></div></div></div>`)
 
-                        // fetch(`https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&units=metric&exclude=alerts&dt=${time}&appid=29d3c41b285caf441bf342de37c4db4d`)
-                        // .then(function (response) {
-                        //       return response.json();
-                        // }).then(function (data){
-                        //       console.log(data)
-                        //       var lat = data[0].lat;
-                        //       var lon = data[0].lon;
-                        //       console.log(`latitude: ${lat}, longitude: ${lon}`)
-                        //       var cityName = data[0].name + ", " + data[0].country;
-                        
-                        //       fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=alerts,minutely&appid=29d3c41b285caf441bf342de37c4db4d`)
-                        //             .then(function (response) {
-                        //                   return response.json();
-                        //             }).then(function (data){
-                                                                              
-                        //                   var weather = data.current.weather[0].main;
-                        //                   console.log("Weather is " + weather);
+                        forecastDays.forEach(date => {    
+                              console.log(date)                          
+                              fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&units=metric&exclude=alerts&date=${date}&appid=29d3c41b285caf441bf342de37c4db4d`)
+                                    .then(function (response) {
+                                          return response.json();
+                                    }).then(function (data){
+                                          console.log(data)
+                                          // An icon representation of weather conditions
+                                          // var forecastweatherIcon = data.current.weather[0].icon;
+                                          // The temperature
+                                          var forecasttemperatureMax = data.temperature.max;
+                                          var forecasttemperatureMin = data.temperature.min;
+                                          // The humidity
+                                          var forecasthumidity = data.humidity.afternoon;
+                                          // The wind speed
+                                          var forecastwindspeed = data.wind.max.speed;
+
+                                          $("#forecastcards").append(`
                                           
-                        //                   // An icon representation of weather conditions
-                        //                   var weatherIcon = data.current.weather[0].icon;
-                        //                   console.log(weatherIcon);
-                        //                   console.log(`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`)
+                                          <div class="card m-2">
+                                                <div class="card-body">
+                                                      <div class="d-flex flex-column justify-content-between align-content-center">
+                                                            <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon">
+                                                            <div class="d-flex flex-column justify-content-evenly">
+                                                                  <h5>${date}</h5>
+                                                            </div>
+                                                      </div>
+                                                      <p class="card-text"><i class="me-2 bi bi-thermometer-half"></i>${forecasttemperatureMin} — ${forecasttemperatureMax}</p>
+                                                      <p class="card-text"><i class="me-2 bi bi-wind"></i> ${forecastwindspeed}</p>
+                                                      <p class="card-text"><i class="me-2 bi bi-droplet-half"></i>${forecasthumidity}</p>
+                                                </div>
+                                          </div>
                                           
-                        //                   // The temperature
-                        //                   var temperature = data.current.temp;
-                        //                   console.log("Temperature is " + temperature);
-                                          
-                        //                   // The humidity
-                        //                   var humidity = data.current.humidity;
-                        //                   console.log("Humidity is " + humidity)
-                                          
-                        //                   // The wind speed
-                        //                   var windspeed = data.current.wind_speed;
-                        //                   console.log("Windspeed is " + windspeed);
-      
-                        //                   currentForecastarea.append("")
-                        //             });
-                        // });            
-            })
-      }
+                                          `)
+
+                                          });
+                                    });            
+                        });
+                  }
 
       FormEl.on("submit", WeatherSearch)
 
