d0b87d85ee58062f6b45b6411b7fc4ba8aac63e0
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-01-29 00:32:35 +0000

    working on 5-day forecast

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,8 @@
 
       var FormEl = $("#search-form")
       var InputEl = $("#search-input")
+
+      var currentForecastarea = $("#today")
       
       function WeatherSearch(event){
             event.preventDefault();
@@ -24,15 +26,17 @@
                   .then(function (response) {
                         return response.json();
                   }).then(function (data){
+                        console.log(data)
                         var lat = data[0].lat;
                         var lon = data[0].lon;
                         console.log(`latitude: ${lat}, longitude: ${lon}`)
+                        var cityName = data[0].name + ", " + data[0].country;
                   
-                        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely&appid=29d3c41b285caf441bf342de37c4db4d`)
+                        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=alerts,minutely&appid=29d3c41b285caf441bf342de37c4db4d`)
                               .then(function (response) {
                                     return response.json();
                               }).then(function (data){
-                                    
+                                                                        
                                     var weather = data.current.weather[0].main;
                                     console.log("Weather is " + weather);
                                     
@@ -52,9 +56,43 @@
                                     // The wind speed
                                     var windspeed = data.current.wind_speed;
                                     console.log("Windspeed is " + windspeed);
+
+                                    currentForecastarea.append(`
+                                    <div class="card">
+                                          <div class="card-body">
+                                          <h3 class="card-title">${cityName}</h3>
+                                          <h5>${date}</h5>
+                              
+                                          <div class="container d-flex flex-nowrap justify-content-start">
+                                          <div class="card m-2" style="width: 15rem;">
+                                                <div class="card-body">
+                                                <h5 class="card-title"><i class="me-2 bi bi-thermometer-half"></i> Temperature </h5>
+                                                <p class="card-text">${temperature}</p>
+                                                </div>
+                                          </div>
+                                          <div class="card m-2" style="width: 15rem;">
+                                                <div class="card-body">
+                                                <h5 class="card-title"><i class="me-2 bi bi-wind"></i> Wind</h5>
+                                                <p class="card-text">${windspeed}</p>
+                                                </div>
+                                          </div>
+                                          <div class="card m-2" style="width: 15rem;">
+                                                <div class="card-body">
+                                                <h5 class="card-title"><i class="me-2 bi bi-droplet-half"></i> Humidity</h5>
+                                                <p class="card-text">${humidity}</p>
+                                                </div>
+                                          </div>
+                                          </div>
+                              
+                                          </div>
+                                    </div>
+                                    `)
                               });
-                  
-                  .append(``)
+
+                        var day2  = currentDate.add(1, 'day')
+                        var day3 = currentDate
+                        
+                        fetch("https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}")
 
 
                   });            
