1c9a4a9e0d48afa741dedbb4d196fb30dbbfe071
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-01-29 17:06:54 +0000

    new api key + working on 5-day forecast again

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,7 @@ function WeatherSearch(event){
       var currentDate = dayjs();
       var date = currentDate.format("dddd DD MMM YYYY");
 
-      fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${SearchQuery}&limit=1&appid=29d3c41b285caf441bf342de37c4db4d`)
+      fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${SearchQuery}&limit=1&appid=d9fb8f659f461f86c935ea25def8363c`)
             .then(function (response) {
                   return response.json();
             }).then(function (data){
@@ -25,7 +25,7 @@ function WeatherSearch(event){
                   var refinedSearch = data[0].name;
                   var cityName = refinedSearch + ", " + data[0].country;
             
-                  fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=alerts,minutely&appid=29d3c41b285caf441bf342de37c4db4d`)
+                  fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=alerts,minutely&appid=d9fb8f659f461f86c935ea25def8363c`)
                         .then(function (response) {
                               return response.json();
                         }).then(function (data){
@@ -71,55 +71,65 @@ function WeatherSearch(event){
                               `)
                         });
 
-                  var forecastDays = [currentDate.add(1, "day"), currentDate.add(2, "day"), currentDate.add(3, "day"), currentDate.add(4, "day"), currentDate.add(5, "day")];
+
+                  // It's 5 days...just have to access it in a certain way. It goes up in steps of 3 hours. 8 indexes = 24 hours and should start from index 7 because of zero index
+                  
+                  var currentUnixTimestamp = dayjs().unix()
+                  
+                  fetch(`https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${currentUnixTimestamp}&appid=d9fb8f659f461f86c935ea25def8363c`)
+
                   
                   fivedayforecastarea.append(`<div class="card text-bg-dark"><div class="card-body"><h4 class="card-title" >5-Day Forecast</h4><div class="container d-flex flex-nowrap justify-content-start" id="forecastcards"></div></div></div>`);
 
-                  forecastDays.forEach(date => {    
-                        fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&units=metric&exclude=alerts&date=${date.format("YYYY-MM-DD")}&appid=29d3c41b285caf441bf342de37c4db4d`)
-                              .then(function (response) {
-                                    return response.json();
-                              }).then(function (data){
-                                    // var forecastweatherIcon = data.current.weather[0].icon;
-                                    var forecasttemperatureMax = data.temperature.max;
-                                    var forecasttemperatureMin = data.temperature.min;
-                                    var forecasthumidity = data.humidity.afternoon;
-                                    var forecastwindspeed = data.wind.max.speed;
-                                    $("#forecastcards").append(`
-                                    <div class="card m-2 text-bg-dark border-secondary" style="width: 15rem;">
-                                          <div class="card-body">
-                                                <div class="d-flex flex-column justify-content-between align-content-center">
-                                                      <img class="mb-3" src="https://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon" style="width: 4rem; height: auto; margin: auto;">
-                                                      <div class="d-flex flex-column justify-content-evenly">
-                                                            <h5>${date.format("dddd DD MMM YYYY")}</h5>
-                                                      </div>
-                                                </div>
-                                                <p class="card-text mb-1"><i class="me-2 bi bi-thermometer-half" style="color: #A26769;"></i>${forecasttemperatureMin} — ${forecasttemperatureMax}°C</p>
-                                                <p class="card-text mb-1"><i class="me-2 bi bi-wind" style="color: #EEEBD0;"></i> ${forecastwindspeed} m/s</p>
-                                                <p class="card-text mb-1"><i class="me-2 bi bi-droplet-half" style="color: #99B2DD;"></i>${forecasthumidity} %</p>
-                                          </div>
-                                    </div>
-                                    `)
+                  // var forecastDays = [currentDate.add(1, "day"), currentDate.add(2, "day"), currentDate.add(3, "day"), currentDate.add(4, "day"), currentDate.add(5, "day")];
+                  
+                  // fivedayforecastarea.append(`<div class="card text-bg-dark"><div class="card-body"><h4 class="card-title" >5-Day Forecast</h4><div class="container d-flex flex-nowrap justify-content-start" id="forecastcards"></div></div></div>`);
 
-                              });
-                        });
+                  // forecastDays.forEach(date => {    
+                  //       fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&units=metric&exclude=alerts&date=${date.format("YYYY-MM-DD")}&appid=d9fb8f659f461f86c935ea25def8363c`)
+                  //             .then(function (response) {
+                  //                   return response.json();
+                  //             }).then(function (data){
+                  //                   // var forecastweatherIcon = data.current.weather[0].icon;
+                  //                   var forecasttemperatureMax = data.temperature.max;
+                  //                   var forecasttemperatureMin = data.temperature.min;
+                  //                   var forecasthumidity = data.humidity.afternoon;
+                  //                   var forecastwindspeed = data.wind.max.speed;
+                  //                   $("#forecastcards").append(`
+                  //                   <div class="card m-2 text-bg-dark border-secondary" style="width: 15rem;">
+                  //                         <div class="card-body">
+                  //                               <div class="d-flex flex-column justify-content-between align-content-center">
+                  //                                     <img class="mb-3" src="https://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon" style="width: 4rem; height: auto; margin: auto;">
+                  //                                     <div class="d-flex flex-column justify-content-evenly">
+                  //                                           <h5>${date.format("dddd DD MMM YYYY")}</h5>
+                  //                                     </div>
+                  //                               </div>
+                  //                               <p class="card-text mb-1"><i class="me-2 bi bi-thermometer-half" style="color: #A26769;"></i>${forecasttemperatureMin} — ${forecasttemperatureMax}°C</p>
+                  //                               <p class="card-text mb-1"><i class="me-2 bi bi-wind" style="color: #EEEBD0;"></i> ${forecastwindspeed} m/s</p>
+                  //                               <p class="card-text mb-1"><i class="me-2 bi bi-droplet-half" style="color: #99B2DD;"></i>${forecasthumidity} %</p>
+                  //                         </div>
+                  //                   </div>
+                  //                   `)
+
+                  //             });
+                  //       });
                         
-                        if (pastSearches.includes(refinedSearch)) {
-                              console.log("This value already exists.")
-                        } else {
-                              pastSearches.unshift(refinedSearch)
-                              localStorage.setItem("historySearches", JSON.stringify(pastSearches)) 
+                  //       if (pastSearches.includes(refinedSearch)) {
+                  //             console.log("This value already exists.")
+                  //       } else {
+                  //             pastSearches.unshift(refinedSearch)
+                  //             localStorage.setItem("historySearches", JSON.stringify(pastSearches)) 
                                                 
-                              recentSearches = [pastSearches[0], pastSearches[1], pastSearches[2], pastSearches[3], pastSearches[4]]
-                              console.log(`Past searches: ${localStorage.setItem("historySearches", JSON.stringify(pastSearches))}`)
-                              console.log(`Recent searches: ${recentSearches}`)
+                  //             recentSearches = [pastSearches[0], pastSearches[1], pastSearches[2], pastSearches[3], pastSearches[4]]
+                  //             console.log(`Past searches: ${localStorage.setItem("historySearches", JSON.stringify(pastSearches))}`)
+                  //             console.log(`Recent searches: ${recentSearches}`)
       
-                              hCEl.empty()
-                              recentSearches.forEach(search => {
-                                    if (!search) {return;}
-                                    hCEl.append(`<button class="btn btn-outline-light m-2" onclick="WeatherSearch()">${search}</button>`)
-                              });
-                        }
+                  //             hCEl.empty()
+                  //             recentSearches.forEach(search => {
+                  //                   if (!search) {return;}
+                  //                   hCEl.append(`<button class="btn btn-outline-light m-2" onclick="WeatherSearch()">${search}</button>`)
+                  //             });
+                  //       }
 
             });
 }
